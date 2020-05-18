819c4abc759e1a10111b520a819910fa22c07aa5
edwardning <edwardning@163.com>
2020-05-18 17:13:37 +0800

    修复不同分辨率训练的bug

--- train.py
+++ train.py
@@ -259,7 +259,7 @@ if __name__ == "__main__":
     class_names = load_classes(data_config["names"])
 
     # Initiate model
-    model = Darknet(opt.model_def).to(device)
+    model = Darknet(opt.model_def, opt.img_size).to(device)
     model.apply(weights_init_normal)
 
     # If specified we start from checkpoint
